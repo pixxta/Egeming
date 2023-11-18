32886da577441f3c96e2a078f8b7e282c8e59b85
pixta <testinfa99@gmail.com>
2023-11-19 02:43:25 +0500

    Добавлена тестовая модель Task

--- egeming/egeming_app/models.py
+++ egeming/egeming_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    """Класс для задания"""
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    points = models.IntegerField()
+    subject_type = models.CharField(max_length=50, default="No subject")
+
+    def __str__(self):
+        return str(self.title) if hasattr(self, 'title') else "No Title"
